926777e3f69a435b137ceb68c82d79588a70923b
Valeriyyy <valeriykutsar18@gmail.com>
2019-11-27 23:39:03 -0800

    Finished bar chart

--- Angular/src/app/charts/bar-chart/bar-chart.component.ts
+++ Angular/src/app/charts/bar-chart/bar-chart.component.ts
@@ -35,9 +35,46 @@ export class BarChartComponent implements OnInit {
 
   ngOnInit() {
     this._salesDataService.getOrders(1, 100)
-      .subscribe( res => {
-        console.log(res['page']['data']);
+      .subscribe(res => {
+        //console.log(res['page']['data']);
+        const localChartData = this.getChartData(<any> res);
+        console.log('localChartData:',localChartData);
+        this.barChartLabels = localChartData.map(x => x[0]).reverse();
+        this.barChartData = [{ 'data': localChartData.map(x => x[1]), label: 'Sales'}];
       });
+  }
+
+  getChartData(res: Response) {
+    this.orders = res['page']['data'];
+    const data = this.orders.map(o => o.total);
+    //console.log('data:', data);
+    const labels = this.orders.map(o => (moment(new Date(o.placed)).format('YY-MM-DD')));
+    const formattedOrders = this.orders.reduce((r, e) => {
+      r.push([moment(e.placed).format('YY-MM-DD'), e.total]);
+      return r;
+    }, []);
+    const p = [];
+    const chartData = formattedOrders.reduce((r, e) => {
+      const key = e[0];
+      if (!p[key]) {
+        p[key] = e;
+        r.push(p[key]);
+      } else {
+        p[key][1] += e[1];
+      }
+      return r;
+    }, []);
+    //console.log('formattedOrders: ', formattedOrders);
+    //console.log('chartData:', chartData);
+
+    return chartData;
+
+    /*const myData = [3, 4, 5].reduce((sum, value) => {
+      console.log(sum, value);
+      return sum + value;
+    }, 0);
+    console.log('mydata:', myData); */
+    //console.log('labels:', labels);
   }
 
 }
